6c09727d0a8c945c99c05691a1fdaf2d378f81d6
Samueldawit20 <samueldawit5343@gmail.com>
2025-03-19 20:35:13 +0300

    Update islower.py

--- Samuel Dawit/String Method/islower()/islower.py
+++ Samuel Dawit/String Method/islower()/islower.py
@@ -1,1 +1,13 @@
 
+
+S = "sami"
+print(S.islower())  # Output: True
+
+S1 = "Sami"
+print(S1.islower())  # Output: False
+
+S2 = "samidawit"
+print(S2.islower())  # Output: True
+
+empty_string = ""
+print(empty_string.islower())  # Output: False
