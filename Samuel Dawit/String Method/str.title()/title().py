61ec16f023da42ae87d24b0b24487bb0ef2544ae
Samueldawit20 <samueldawit5343@gmail.com>
2025-03-21 21:46:48 +0300

    Update title().py

--- Samuel Dawit/String Method/str.title()/title().py
+++ Samuel Dawit/String Method/str.title()/title().py
@@ -1,1 +1,8 @@
 
+sentence = "sami is learning to do python"
+
+# Convert to title case
+title_case_sentence = sentence.title()
+
+# Print the result
+print(title_case_sentence)  # Output: "Sami Is Learning To Do Python"
